18a7241cf9714668e621dd955f0f43b250e3cbad
KimKyungTae <pavve0712@daum.net>
2020-09-24 00:40:23 +0900

    Update views.py

--- Todo_project/api/views.py
+++ Todo_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import TodoSerialzer
+from .models import TodoGroup, Todo
 # Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerialzer
